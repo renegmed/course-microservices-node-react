fbf8ad05238a7d376ad9d3f61f400511f95a83f9
rene <renegmed@yahoo.com>
2021-03-29 06:16:29 -0400

    sec05-architect-multi-service-apps: hosts file and sec warnin #107 [end of section]

---
+++ 2-ticketing/auth/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');  // curl localhost:3000/api/users/currentuser
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000xxxx');
+});
