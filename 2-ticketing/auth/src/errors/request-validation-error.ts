ba67fd37427abe5d9ce3976ac648378deab68fc3
Rene Medalla <renemed@pop-os.localdomain>
2021-03-29 21:23:41 -0400

    sec07-response normalization strategies: async error handling #137 [end of section]

--- 2-ticketing/auth/src/errors/request-validation-error.ts
+++ 2-ticketing/auth/src/errors/request-validation-error.ts
@@ -1,16 +1,35 @@
-import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+import { ValidationError } from 'express-validator';
+/*
+
+interface CustomError {
+    statusCode: number;
+    serializeErrors(): {
+        message: string;
+        field?: string;
+    }[];
+}
+
+*/
+export class RequestValidationError extends CustomError {  // implements CustomError
     
+    statusCode = 400; 
     constructor(public errors: ValidationError[]) {
 
         // NOTE: private is equivalent to 
         // errors: ValidationError[]
         // this.errors = errors 
 
-        super();
+        super('Invalid parameter');
 
         // Only because wea reextending a built in class
         Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+    
+    serializeErrors() {
+        return this.errors.map(err => {
+            return { message: err.msg, field: err.param };
+        });
     }
 }
--- 2-ticketing/auth/src/middlewares/error-handler.ts
+++ 2-ticketing/auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
     err: Error, 
@@ -10,20 +9,10 @@ export const errorHandler = (
     ) => {
         // console.log(err.message);
 
-        if (err instanceof RequestValidationError) {
-            //console.log('Handling this error as a request validation error');
-            const formattedErrors = err.errors.map(error => {
-                return { message: error.msg, field: error.param }
-            });
-
-            return res.status(400).send({ errors: formattedErrors });   // array of errors
+        if (err instanceof CustomError) {           
+            return res.status(err.statusCode).send({ errors:  err.serializeErrors() });   // array of errors
         }
-
-        if (err instanceof DatabaseConnectionError) {
-            //console.log('handling this error as a db connection error');
-            return res.status(500).send({ errors: [{ message: err.reason }] });
-        }
-         
+ 
         res.status(400).send({
            errors: [{ message: 'Something went wrong' }]
         });
