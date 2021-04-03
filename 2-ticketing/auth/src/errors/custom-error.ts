4a6116822e31866963bfb03f8062a5ebaf45fc9b
Rene Medalla <renemed@pop-os.localdomain>
2021-04-03 09:53:53 -0400

    sec10: testing isolated microservices - a few dependencies #185 [start of section]

--- 2-ticketing/auth/src/errors/custom-error.ts
+++ 2-ticketing/auth/src/errors/custom-error.ts
@@ -4,7 +4,8 @@ export abstract class CustomError extends Error {
     constructor(message: string) {
         super(message);
 
-        Object.setPrototypeOf(this, CustomError.prototype);
+        Object.setPrototypeOf(this, CustomError.prototype); 
+        // this - The object to change its prototype, CustomError.prototype -  The value of the new prototype or null
     }
 
     abstract serializeErrors(): { message: string; field?: string }[];
