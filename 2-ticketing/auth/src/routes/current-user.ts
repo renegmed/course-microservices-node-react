0c12ac1fde3932585ddc322e1112d39ffbf3d220
Rene Medalla <renemed@pop-os.localdomain>
2021-04-03 02:05:02 -0400

    sec09-authentication strategies and options - returning the current user #176

--- 2-ticketing/auth/src/routes/current-user.ts
+++ 2-ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!')
+    if (!req.session?.jwt) { // ? same as  if (!req.session || !req.session.jwt) 
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify(
+            req.session.jwt, 
+            process.env.JWT_KEY! // ! is already defined previously
+        );
+        res.send({ currentUser: payload });
+    } catch (err) {
+        res.send({ currentUSer: null });
+    }
 });
 
 export  { router as currentUserRouter };
