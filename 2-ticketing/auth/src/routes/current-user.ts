0dab95e2f7a43b5bf7cc8d85968e628ebd3d599e
Rene Medalla <renemed@pop-os.localdomain>
2021-04-03 03:20:58 -0400

    sec09-authentication strategies and options - create current user middleware & augment type def #179

--- 2-ticketing/auth/src/routes/current-user.ts
+++ 2-ticketing/auth/src/routes/current-user.ts
@@ -1,22 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 
+import { currentUser } from '../middlewares/current-user';
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    if (!req.session?.jwt) { // ? same as  if (!req.session || !req.session.jwt) 
-        return res.send({ currentUser: null });
-    }
-
-    try {
-        const payload = jwt.verify(
-            req.session.jwt, 
-            process.env.JWT_KEY! // ! is already defined previously
-        );
-        res.send({ currentUser: payload });
-    } catch (err) {
-        res.send({ currentUSer: null });
-    }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+    res.send({ currentUser: req.currentUser || null });  
 });
 
 export  { router as currentUserRouter };
