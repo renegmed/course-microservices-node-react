4f33766d58be3dba1105077229ff6f12f5334b4a
Rene Medalla <renemed@pop-os.localdomain>
2021-04-07 16:33:24 -0400

    sec11: server-side-render react app: passing through the cookies #222

--- 2-ticketing/client/pages/index.js
+++ 2-ticketing/client/pages/index.js
@@ -4,28 +4,26 @@ import axios from 'axios';
 // for nextjs, we are not allowed to fetch inside this component only on initialization only
 const LandingPage = ({ currentUser }) => {
      
-    console.log(currentUser);
+    console.log('...currentUser:\n', currentUser);
      
     return <h1>Landing Page</h1>
 };
 
 // Execute on the server
 // this is nextjs specific, thiis is where we can fetch data 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
         //console.log('I am on the server!');
-
+ 
     if (typeof window === 'undefined') {
         // we are on the server!
-        // request should be mad to http://ingress-nginx.default.svc.cluster.local/api/users/currentuser
+        // request should be mad to http://ingress-nginx-controller.default.svc.cluster.local/api/users/currentuser
         const { data } = await axios.get (  
             'http://ingress-nginx-controller.kube-system.svc.cluster.local/api/users/currentuser',
              {
-                headers: {
-                    Host: 'ticketing.dev'
-                }
+                headers: req.headers 
             }
         );
-
+        // NOTE: host 'ticketing.dev' found in infra/k8s/ingress-srv.yaml 
         return data;
 
     } else {
